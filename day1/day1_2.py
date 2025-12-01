d428dbf709467b43e071a49044456929974a0bd9
Fernando Barros <fbarros@hovione.com>
2025-12-01 10:26:57 -0500

    Day 1 completed

---
+++ day1/day1_2.py
@@ -0,0 +1,29 @@
+def parse_input(fname):
+    with open(fname) as f:
+        lines = f.readlines()
+
+    steps = []
+    for l in lines:
+        l = l.strip("\n")
+        d, n = l[0], int(l[1:])
+        steps.append({'dir': d, 'n': n})
+    return steps
+
+steps = parse_input('day1/input.txt')
+
+pos = 50
+count = 0
+
+c0 = 0
+for s in steps:
+    d_s = s['dir']
+    d = 1 if d_s == 'R' else -1 
+    n = s['n']
+    pos = pos + d*n
+    c = pos // 100
+    count += abs(c - c0)
+    c0 = c
+
+    print(pos)
+
+print(f"Count = {count}")
